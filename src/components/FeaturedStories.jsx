151159a5354d5638160e2f3a7250ffc629978c8c
aijaz ali <you@example.com>
2019-02-22 17:20:34 +0500

    gallery complete1

--- src/components/FeaturedStories.jsx
+++ src/components/FeaturedStories.jsx
@@ -16,39 +16,7 @@ class FeaturedStories extends Component {
 
     return (
       <div >
-
-        {/* <div>
-        <Container>
-        <Row>
-          <Col lg>
-            <div className="d-flex justify-content-between">
-              <h3>{this.props.leftData}</h3>
-              <h6>{this.props.rightData}</h6>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          {
-            this.props.pics.map((anObjectMapped, index)  => (
-           
-                <Col md={3} key={anObjectMapped.id} ><Image src={anObjectMapped.pic} width="100%" height="100%"/></Col>
-                
-          ))
-          }  
-        </Row>
-        </Container>
-        </div> */}
         
-        {/* <Lightbox
-          images={this.props.pics.map((pic)  => ({
-            src: pic.pic
-
-          }))}
-          isOpen={this.state.lightboxIsOpen}
-          onClickPrev={this.gotoPrevLightboxImage}
-          onClickNext={this.gotoNextLightboxImage}
-          onClose={this.closeLightbox}
-        /> */}
         <div className="d-flex justify-content-between" style={{paddingLeft: "10px", paddingRight: "10px"}}>
               <h3>{this.props.leftData}</h3>
               <h6>{this.props.rightData}</h6>
